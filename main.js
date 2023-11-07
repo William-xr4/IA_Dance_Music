b94ba63f731144633bde469d62141ca23636b44e
William-xr4 <108703320+William-xr4@users.noreply.github.com>
2023-11-07 11:35:41 -0300

    I.A. Dance Music-I.A.D.M.
    Atualizando meu site!

--- main.js
+++ main.js
@@ -1,47 +1,65 @@
-var img="";
-var detect_object="";
-var status="";
-var objects=[];
-
+var song1="";
+var song2="";
+song_status1="";
+song_status2="";
+var scoreRightWrist=0;
+var scoreLeftWrist=0;
 
 function preload(){
-    img=loadImage("car.jpg");
+    song1=loadSound("BEETHOVEN.mp3");
+    song2=loadSound("Marshmello - Alone.mp3");
 }
-
 function setup(){
-    canvas=createCanvas(1000, 600);
+    canvas=createCanvas(600, 400);
     canvas.center();
-    detect_object=ml5.objectDetector('cocossd', modelLoaded);
-    document.getElementById("Btn_status").innerHTML="Status: Detectando objeto(s)";
+    video=createCapture(VIDEO);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
-
 function modelLoaded(){
-    console.log("Modelo carregado");
-    status=true;
-    detect_object.detect(img, gotResults);
+    console.log('PoseNet Is Initialized');
 }
-
-function gotResults(error, results){
-    if(error){
-        console.error(error);
-    }
-        console.log(results);
-        objects=results;
-}
-
 function draw(){
-    image(img, 0, 0, 1000, 600);
-
-    if(status != ""){
-
-        for(var inc=0; inc<objects.length; inc++){
-
-            document.getElementById("Btn_status").innerHTML="status:Objeto(s) detectado(s).";
-            fill("#00FFFF");
-            percent=floor(objects[inc].confidence*100);
-            text(objects[inc].label+" "+percent+"%", objects[inc].x, objects[inc].y);
-            noFill();
-            rect(objects[inc].x, objects[inc].y, objects[inc].width, objects[inc].height);
+    image(video, 0, 0, 600, 400);
+    song_status1=song1.isPlaying();
+    song_status2=song2.isPlaying();
+    fill("red");
+    stroke("yellow");
+    if(scoreRightWrist>0.2){
+        circle(rightWristX, rightWristY, 50);
+        song2.stop();
+        if(song_status1==false){
+            song1.play();
         }
     }
+    if(scoreLeftWrist>0.2){
+        circle(leftWristX, leftWristY, 50);
+        song1.stop();
+        if(song_status2==false){
+            song2.play();
+        }
+    }
+}
+function Start_Music(){
+    song1.play();
+}
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+	console.log(results);
+	scoreRightWrist =  results[0].pose.keypoints[10].score;
+	scoreLeftWrist =  results[0].pose.keypoints[9].score;
+	console.log("scoreRightWrist = " + scoreRightWrist + " scoreLeftWrist = " + scoreLeftWrist);
+	
+	rightWristX = results[0].pose.rightWrist.x;
+	rightWristY = results[0].pose.rightWrist.y;
+	console.log("rightWristX = " + rightWristX +" rightWristY = "+ rightWristY);
+
+	leftWristX = results[0].pose.leftWrist.x;
+	leftWristY = results[0].pose.leftWrist.y;
+	console.log("leftWristX = " + leftWristX +" leftWristY = "+ leftWristY);
+		
+  }
 }
